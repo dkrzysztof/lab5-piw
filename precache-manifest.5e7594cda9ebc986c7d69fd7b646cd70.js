74b82d488cedcc355296ca519f8b7f821d1adb8f
Krzysztof <dydakkrzysztof@gmail.com>
2020-06-06 20:22:27 +0200

    Updates

---
+++ precache-manifest.5e7594cda9ebc986c7d69fd7b646cd70.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf010babc65d3edf461dbeae546c1ef9",
+    "url": "/lab5-piw/index.html"
+  },
+  {
+    "revision": "c9216fb2757f2e11ebd7",
+    "url": "/lab5-piw/static/css/main.e051cee0.chunk.css"
+  },
+  {
+    "revision": "6a7c11309794b8e5929c",
+    "url": "/lab5-piw/static/js/2.31382ebd.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/lab5-piw/static/js/2.31382ebd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c9216fb2757f2e11ebd7",
+    "url": "/lab5-piw/static/js/main.b348c100.chunk.js"
+  },
+  {
+    "revision": "bc295362b3ea81865f6b",
+    "url": "/lab5-piw/static/js/runtime-main.755498ee.js"
+  },
+  {
+    "revision": "8be1620010705154fe07eff863584552",
+    "url": "/lab5-piw/static/media/logo.8be16200.png"
+  }
+]);
